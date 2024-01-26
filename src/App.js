637df625ae27a095110239f6d414e55fad40f139
gutsyzen <kumarpraveen43281@gmail.com>
2024-01-26 15:52:21 +0530

    REACTday3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Layout from "./Layout"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Service from "./pages/Service"
-
-export default function App() {  
+import React from 'react'
+import Counter from './Counter'
+export default function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Layout/>}>
-    <Route index element={<Home/>} />
-    <Route path="about" element={<About/>} />
-    <Route path="contact" element={<Contact/>} />
-    <Route path="service" element={<Service/>} />
-    
-    </Route>
-    </Routes>
-    </BrowserRouter>
+    <div>
+      <Counter/>
+    </div>
   )
-} 
+}
